d0fd69ad9432c0f8ae9e78dd62be79d281ad415c
luispaiva40200280 <luispaiva8936@gmail.com>
2022-01-16 00:45:19 +0000

    first commit

---
+++ js/admin.js
@@ -0,0 +1,8 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("mySidebar").style.width = "100%";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+const perfil_btn = document.querySelector('.btn-primary')
+const user_name = document.getElementById('nome_login').Value //login
+const name_regitro = document.getElementById('nome_registro').Value
+
+
+
