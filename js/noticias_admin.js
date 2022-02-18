384130bd797bbf080fd1b0cb5840fc36a78d6d7c
luispaiva40200280 <luispaiva8936@gmail.com>
2022-02-18 22:24:14 +0000

    ok

--- js/noticias_admin.js
+++ js/noticias_admin.js
@@ -1,7 +1,15 @@
-let form = document.getElementsByTagName('form')
 
-form.addEventListener("submit", event =>{
-    alert('a noticia fo adicionada com sucesso')
+let form = document.getElementsByTagName('form')
+let submit = document.getElementById('adiciona_noticia')
+
+function adicionar_noticia(e){
+    alert('a noticia foi adicionada com sucesso')
+    console.log('noticia adicionada ');
+
+    let noticia = document.getElementsByClassName('container-noticias')
+    noticia.replaceChild(form)
+
+    //this.parentNode.parentNode.remove();
     
-    event.preventDefault() 
-})
+    e.preventDefault() 
+}
--- js/usersadmin.js
+++ js/usersadmin.js
@@ -1,5 +1,18 @@
-let remover = document.getElementsByTagName("button")
+let  btn = document.getElementsByClassName("remover_utilizador")
 
-remover.onclick =  function(){
-    console.log('entrar');
+function remover(e){
+    alert('tem certeza que quer eleminar este utilizador')
+    
+    for (let index = 0; index < btn.length; index++) {
+        
+        btn[index].parentNode.parentNode.remove()
+        
+    }
+    e.preventDefault()
 }
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', remover )
+
+}
+
